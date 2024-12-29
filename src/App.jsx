4a46b0646a52d7a646df8286d102507260542eb5
psattri87 <psattri87@gmail.com>
2024-12-29 23:09:12 +0530

    time format correction-3

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ function App() {
   const [isRunning, setIsRunning] = useState(false);
 
   // const hour = Math.floor(time / 360000);
-  const minute = Math.floor((time / 6000) % 60);
-  const second = Math.floor((time /100)% 60);
+  const minute = Math.floor(time / 60);
+  const second = Math.floor(time % 60);
+  console.log(minute, ":", second)
 
   useEffect(() => {
     let intervalId;
     if (isRunning) {
       // setting time from 0 to 1 every second
-      intervalId = setInterval(() => setTime(time + 1), 10);
+      intervalId = setInterval(() => setTime((time)=>time + 1), 1000);
     }
     return () => clearInterval(intervalId);
   }, [isRunning, time]);
